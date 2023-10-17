01a8ccfe64ea469f7185b39424acc90a42319e17
kristijanH1998 <kristijan.hornung1@gmail.com>
2023-10-17 16:47:39 -0700

    added click event listeners for all rows of resume table

--- js-script.js
+++ js-script.js
@@ -1,10 +1,12 @@
-var popupLink = document.getElementById("popup-link");
-  var popupWindow = document.getElementById("popup-window");
-  var closeButton = document.getElementById("close-button");
-  popupLink.addEventListener("click", function(event) {
+const popupLinks = document.getElementsByClassName("popup-links");
+var popupWindow = document.getElementById("popup-window");
+var closeButton = document.getElementById("close-button");
+for(let i = 0; i < popupLinks.length; i++){
+  popupLinks[i].addEventListener("click", function(event) {
     event.preventDefault();
     popupWindow.style.display = "block";
   });
   closeButton.addEventListener("click", function() {
     popupWindow.style.display = "none";
   });
+}
