3418338525642e0e7c00a3f8a689552bb8390806
kristijanH1998 <kristijan.hornung1@gmail.com>
2023-10-22 01:10:14 -0700

    email sending form with PHP first attempt

--- email-form.php
+++ email-form.php
@@ -1,1 +1,6 @@
-<?phpif($_POST["message"]) {mail("your@email.address", "Here is the subject line",$_POST["insert your message here"]. "From: an@email.address");}?>
+<?php
+if($_POST["message"]) {
+    mail("kristijan.hornung1@gmail.com", "Here is the subject line",
+    $_POST["insert your message here"]. "From: kristijan.hornung1@gmail.com");
+    }
+?>
